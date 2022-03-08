e553557e552c47c11bb15b2da5208706f591e8cd
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-03-08 18:06:31 -0500

    connecting mogodb cloud data base

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('mogodb database is now connected');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 const app = express();
 
 app.get('/api/products', (req, res) => {
