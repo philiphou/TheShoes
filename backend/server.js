cbd61e666e77b2c1e508bf4c6f70cd77fafef359
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-03-09 15:10:05 -0500

    add npm build to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -30,6 +30,12 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`the server is ready at http://localhost:${port}`);
