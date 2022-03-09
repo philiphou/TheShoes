b30428ffa1c13a6abaf629be73675b87ac74a666
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-03-09 15:46:27 -0500

    debug deploy

--- frontend/src/pages/HomeScreen.jsx
+++ frontend/src/pages/HomeScreen.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Product from '../components/Product';
+import Product from './../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
