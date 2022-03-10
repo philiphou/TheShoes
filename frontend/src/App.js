c09c297236830f56b2a30ed66c6677eff4bd3c4f
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-03-10 18:22:37 -0500

    styling2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
   };
   return (
     <BrowserRouter>
-      <div className="d-flex flex-row body-container">
+      <div className="d-flex flex-column body-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
